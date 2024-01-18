f1b15828bce1bc187a8ff3085f82c139a5693775
gelutz <gabriel.lutz5@gmail.com>
2024-01-18 14:27:50 -0300

    adicionando primeiro teste com Jasmine

---
+++ src/tests/services/ProductService.spec.ts
@@ -0,0 +1,13 @@
+import { ProductService } from "../../services/ProductService";
+
+describe("ProductService", () => {
+    describe("listAll", () => {
+        let productService: ProductService;
+        beforeEach(() => {
+            productService = new ProductService();
+        });
+        it("should return an array", async () => {
+            return expectAsync(productService.listAll()).toBeResolvedTo([]);
+        });
+    });
+});
