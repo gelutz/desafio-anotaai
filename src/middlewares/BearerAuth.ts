4863e11798bfad40f1940d0aeb0c2cffa4e4f62b
gelutz <gabriel.lutz5@gmail.com>
2024-01-25 14:32:00 -0300

    fixed authentication middleware

--- src/middlewares/BearerAuth.ts
+++ src/middlewares/BearerAuth.ts
@@ -1,26 +1,38 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JsonWebTokenError } from "jsonwebtoken";
 
+const messages = {
+    notAuthorized: "Não está autorizado",
+    tokenExpired: "O token está expirado. Tente fazer login novamente",
+    tokenError: "Houve um erro com o seu token. Tente fazer login novamente",
+};
+
 export function bearerAuth(req: Request, res: Response, next: NextFunction): Response | void {
     const { authorization } = req.headers;
 
     if (typeof authorization != "string") {
-        return res.status(401).send({ message: "Não está autorizado" });
+        return res.status(401).send({ message: messages.notAuthorized });
     }
 
     const token = authorization.replace("Bearer ", "");
 
     if (token === "") {
-        return res.status(401).send({ message: "Não está autorizado" });
+        return res.status(401).send({ message: messages.notAuthorized });
     }
 
     try {
         jwt.verify(token, process.env.JWT_KEY as string);
     } catch (err) {
         if (err instanceof JsonWebTokenError) {
-            if (err.message === "Token expirado") {
-                return res.status(401).send({ message: "O token está expirado. Favor refazer o login." });
+            if (err.message.includes("expired")) {
+                return res.status(401).send({ message: messages.tokenExpired });
             }
+
+            if (err.message.includes("malformed")) {
+                return res.status(401).send({ message: messages.notAuthorized });
+            }
+
+            return res.status(401).send({ message: messages.tokenError });
         }
     }
 
